380d414b9e10858388823755d5da3ad619225680
ofer <oferc008@gmail.com>
2023-10-22 22:03:53 +0300

    added CRUD rest framwork

--- My_Store/admin.py
+++ My_Store/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Product,Cart,CartItem
+from .models import Product,Cart,CartItem,CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'age', 'city')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'password1', 'password2', 'age', 'city'),
+        }),
+    )
 
 # Register your models here.
-
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItem)
--- My_Store/models.py
+++ My_Store/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True)
+    city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -17,7 +26,7 @@ class Product(models.Model):
 class Cart(models.Model):
     is_paid = models.BooleanField(default=False)
     create_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Cart for {self.user.username}"
