9673324e0ddba29d6ef63f24d4955cc5ba6f5e9a
ofer <oferc008@gmail.com>
2023-11-24 09:31:32 +0200

    added react app deploy seeting

--- Store_Proj/settings.py
+++ Store_Proj/settings.py
@@ -47,6 +47,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:9000",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "https://oferkarp.github.io/react-store/",
 ]
 
 MIDDLEWARE = [
