913a28e1a2b893cbe21853838b6ae55a869b034a
ofer <oferc008@gmail.com>
2023-11-28 21:28:06 +0200

    update seeting

--- Store_Proj/settings.py
+++ Store_Proj/settings.py
@@ -59,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:9000",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "http://localhost:4000",
     "https://oferkarp.github.io",
 ]
 
